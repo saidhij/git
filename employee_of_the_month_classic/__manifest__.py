8632d5ff964532518650cdb73ed0d712719578cb
saidhij <said_hij@yahoo.fr>
2017-11-13 09:31:18 +0000

    Employee certificates
    Certificates for you employees

---
+++ employee_of_the_month_classic/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Classic Employee of the Month certificate',
+    'summary': 'Print from odoo your employee of the month certificate',
+    'category': 'Reporting',
+    'author': 'Said Hijaoui(rhfree.com)',
+    'website': "http://rhfree.com",
+    'price': 40,
+    'currency': 'EUR',
+    'depends': ['base'],
+    'version': 'Classic1.0',	
+
+    'description': """Employee of The Month.
+      ======================
+    - This application will help you print the certificate of the the employee of the month in a classic design
+	- No time waiste, directly from employee menu. 
+ 
+    """,
+	
+    'data': [
+        'report/classic_certificate_template.xml',
+        'report/classic_certificate_report.xml',
+    ],
+    'css': ['static/src/css/report.css'],
+	
+    "installable": True,
+    # "auto_install": False
+    "images":['static/description/Banner.png'],
+}

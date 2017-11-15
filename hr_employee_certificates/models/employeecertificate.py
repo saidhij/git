45dbc2b466dbd0d06bfd130fee54fbc38c2f8b1f
saidhij <said_hij@yahoo.fr>
2017-11-15 00:15:58 +0000

    V10 for certificates
    V10 for certificates

---
+++ hr_employee_certificates/models/employeecertificate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import models, fields, api
+
+class EmployeDocuments(models.AbstractModel):
+
+
+    @api.model
+    def get_report_values(self,docids, data=None):
+        records = self.env['hr.employee'].browse(docids)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'hr.employee',
+            'docs': records,
+            'data': data,
+
+        }
+
+

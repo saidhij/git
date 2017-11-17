09954b1bc6e88e1823af3c6f4b2a07642a35a9f4
saidhij <said_hij@yahoo.fr>
2017-11-17 16:47:04 +0000

    contract ref
    Add a reference to a contract

---
+++ hr_contract_sequence/models/__init__.py
@@ -0,0 +1,5 @@
+﻿# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import hr_employee_sequence
+
---
+++ hr_contract_sequence/models/hr_employee_sequence.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models,api, _
+from odoo.exceptions import ValidationError
+
+
+	
+class hr_contract(models.Model):
+    _inherit = 'hr.contract'
+	
+	
+    name = fields.Char('Contract Reference', required=False,readonly=True, copy=False, default='Autofill')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('number', 'Autofill') == 'Autofill':
+            vals['name'] = self.env['ir.sequence'].next_by_code('contract.ref')
+        return super(hr_contract, self).create(vals)
+	
+  
+
+		
+		
+
+		
+		
+		
+	

244a236c14c32d7186dd937478f24a66ef864ca7
saidhij <said_hij@yahoo.fr>
2017-12-13 11:30:45 +0000

    NY
    NY t

---
+++ odoo_blue_ny_theme/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Majorelle blue theme NY2018',
+    'summary': 'If you like blue,try this backend theme',
+    'category': 'Themes/Backend', 
+    'author': 'sitepasmal.com',
+    'website': "http://sitepasmal.com",
+    'price': 200,
+    'currency': 'EUR',
+    'depends': ['base'],
+    'version': '1.0',	
+
+    'description': """Odoo Majorelle blue backend theme theme with Happy New year.
+      ======================
+    - Nice looking blue theme for odoo .
+	- Enjoy it. Your eyes will be happy .
+	- After you install the theme. Please restart odoo and update the application.
+    """,
+	
+    'data': [
+           'views/custom_view.xml'
+    ],
+    'css': ['static/src/css/styles.css'],
+	
+    "installable": True,
+    "auto_install": False,
+    'images':[
+        'static/description/banner2.png',
+        'static/description/screenshot2.jpg',
+	 ],
+}

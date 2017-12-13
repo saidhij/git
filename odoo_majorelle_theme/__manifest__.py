d1172c1a62c0c2a9a5c062a8ec088fe86f7cedde
saidhij <said_hij@yahoo.fr>
2017-12-13 10:44:25 +0000

    blue t
    blue t

---
+++ odoo_majorelle_theme/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Majorelle blue theme',
+    'summary': 'If you like blue,try this backend theme',
+    'category': 'Themes/Backend', 
+    'author': 'sitepasmal.com',
+    'website': "http://sitepasmal.com",
+    'price': 200,
+    'currency': 'EUR',
+    'depends': ['base'],
+    'version': '1.0',	
+
+    'description': """Odoo Majorelle blue backend theme theme.
+      ======================
+    - Nice looking blue theme for odoo .
+	- Enjoy it. Your eyes will be happy .
+	- After you install the theme. Please restart odoo and update the application.
+    """,
+	
+    'data': [
+           'views/custom_view.xml'
+    ],
+    'css': ['static/src/css/styles.css'],
+	
+    "installable": True,
+    "auto_install": False,
+    'images':[
+        'static/description/banner.png'
+        'static/description/screenshot.png'
+	 ],
+}
